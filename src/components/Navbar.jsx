e7d62b0245df0817a61bee9bd97b21b763815673
fauzirahmat89 <fauzirahmat89@gmail.com>
2023-11-19 20:28:13 +0700

    tambah href di navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
             <img className='logo w-16' src="images/logo.png" alt="" />
           </div>
           <div className='navbar-nav flex items-center font-Poppins'>
-            <a className='mx-4 text-lg'>Home</a>
-            <a className='mx-4 text-lg'>Service</a>
-            <a className='mx-4 text-lg'>About</a>
-            <a className='mx-4 text-lg'>Contact</a>
+            <a className='mx-4 text-lg' href='/Home'>Home</a>
+            <a className='mx-4 text-lg' href='/'>Service</a>
+            <a className='mx-4 text-lg' href='/'>About</a>
+            <a className='mx-4 text-lg' href='/'>Contact</a>
           </div>
           <div className='flex items-center'>
             <button className='font-Poppins px-3 h-8 rounded-md text-white' >Sign Up</button>
