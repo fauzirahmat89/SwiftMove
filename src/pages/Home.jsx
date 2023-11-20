1abc97cf39f2cd3f3082447471a6dc3d9689f5e1
andiseo <andiseyo50@gmail.com>
2023-11-20 14:49:43 +0700

    modified card and home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,20 +8,20 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <img className='mx-auto' src="images/banner.png" alt="banner" />
-      <div className="header text-center font-Poppins font-medium text-3xl my-12">Top Partners</div>
+      <img className='w-screen justify-center' src="images/banner.png" alt="banner" />
+      <div className="header text-center font-Poppins font-medium text-4xl my-12">Top Partners</div>
       <div className='partner container mx-auto flex justify-center'>
         <Card />
         <Card />
         <Card />
       </div>
-      <div className="header text-center font-Poppins font-medium text-3xl my-12">Additional Service</div>
-      <div className="content container mx-auto flex justify-center gap-10">
+      <div className="header text-center font-Poppins font-medium text-4xl my-12">Additional Service</div>
+      <div className="content container mx-auto flex justify-center gap-12">
         <div className="text flex flex-col columns-lg gap-5 my-auto">
-          <div className="text-header font-Poppins text-2xl">Extra protection of your goods <br /> with wooden packaging.</div>
-          <div className="text-body font-Poppins text-base text-justify w-96">We recruit professionals to improve the efficiency of your time and energy when moving house, no need to worry because we will deliver your goods to a new place safely.</div>
+          <div className="text-header font-Poppins text-3xl">Extra protection of your goods <br /> with wooden packaging.</div>
+          <div className="text-body font-Poppins text-xl text-justify w-96">We recruit professionals to improve the efficiency of your time and energy when moving house, no need to worry because we will deliver your goods to a new place safely.</div>
         </div>
-        <img className="w-96 items-center" src="images/wooden.jpg" alt="" />
+        <img className="items-center" style={{width: '500px'}} src="images/wooden.jpg" alt="" />
       </div>
       <Footer1 />
     </div>
