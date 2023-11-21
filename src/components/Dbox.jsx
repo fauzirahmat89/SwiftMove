2d96ecb08fd8cbe51627d7badce268d180744fb8
andiseo <andiseyo50@gmail.com>
2023-11-21 17:46:20 +0700

    Modified Dbox adn Review

--- src/components/Dbox.jsx
+++ src/components/Dbox.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 
 const Dbox = () => {
   return (
-    <div className='flex items-center justify-center my-12'>
-        <div className='dbox flex items-center p-7' style={{width: '1240px'}}>
-            <p className=' text-black mx-auto font-Poppins text-xl'>Moving Packages :<br/>
-            Offers a range of home moving packages tailored to the customer's needs, catering to both small and large-scale relocations.<br/>
-            Specialized packages available for long-distance or inter-city moves.<br/>
-            Trained and experienced team proficient in executing the home moving process.<br/>
-            Team members equipped with necessary tools for secure packing and transportation<br/>
-            Comprehensive packing and unpacking services using high-quality packing materials.<br/>
-            Systematic identification and labeling for easy retrieval of items during unpacking.<br/>
-            Dedicated home moving vehicles with safety equipment to prevent damage to belongings during transit.<br/>
-            Optional insurance coverage for added peace of mind.<br/>
-            </p>
-        </div>
+    <div className='dbox flex items-center justify-center mx-auto p-7 rounded-md my-12' style={{width: '1240px'}}>
+      <div className="text-black w-full text-justify text-base">Moving Packages :<br />
+      Offers a range of home moving packages tailored to the customer's needs, catering to both small and large-scale relocations.<br/>
+      Specialized packages available for long-distance or inter-city moves.<br/>
+      Trained and experienced team proficient in executing the home moving process.<br/>
+      Team members equipped with necessary tools for secure packing and transportation<br/>
+      Comprehensive packing and unpacking services using high-quality packing materials.<br/>
+      Systematic identification and labeling for easy retrieval of items during unpacking.<br/>
+      Dedicated home moving vehicles with safety equipment to prevent damage to belongings during transit.<br/>
+      Optional insurance coverage for added peace of mind</div>
     </div>
 
   )
