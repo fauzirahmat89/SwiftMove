f8db786c639874a780162c2a9d2b2dc25aa981e5
fauzirahmat89 <fauzirahmat89@gmail.com>
2023-11-22 20:12:05 +0700

    Memperbaiki halam login dan daftar

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Login from './Auth/login'
-import Register from './Auth/register'
+// import Login from './Auth/login'
+// import Register from './Auth/register'
 import Sidebar from './components/Sidebar'
 import Detail from './pages/Detail'
 import Booking from './pages/Booking'
 import HomeMitra from './Mitra/HomeMitra'
 import Service from './pages/Service'
+import Sign from './pages/Sign'
+import SignUp from './pages/SignUp'
 // import Regist from './pages/Regist'
 
 function App() {
@@ -21,8 +23,10 @@ function App() {
           <Route exact path="/" element={<Home/>} />
           <Route  path="/about" element={<About/>} />
           <Route  path="/contact" element={<Contact/>} />
-          <Route path="/Auth/login" element={<Login />} />
-          <Route path="/Auth/register" element={<Register />} />
+          {/* <Route path="/Auth/login" element={<Login />} />
+          <Route path="/Auth/register" element={<Register />} /> */}
+          <Route path='/Sign' element={<Sign/>}/>
+          <Route path='/SignUp' element={<SignUp/>}/>
           <Route path="/components/Sidebar" element={<Sidebar/>} />
           <Route path="/Home" element={<Home />}></Route>
           <Route path="/Detail" element={<Detail/>}></Route>
